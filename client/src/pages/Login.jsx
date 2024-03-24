0bcaaf57c73f07ebd94bf08d231bdcd16c3c8733
Himanshi Singh <shimanshi9911@gmail.com>
2024-03-24 23:35:57 +0530

    login & register comp

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    const [user,setUser] = useState({
+      email:'johndoe@yahoo.com',
+      password:'1234567890',
+    })
+    //handling input
+    const handleInput = (e) => {
+      console.log(e);
+      console.log('ahgdj');
+      const name = e.target.name;
+      const val = e.target.value;
+      setUser({...user,[name]:val})
+  
+    }
+    return (<>
+    <main>
+      <div className="section-login">
+        <div className="login-form">
+          <h1>Login form</h1>
+          <form >
+            <label htmlFor="email">email</label>
+            <input type="email" name="email" id="email" 
+            required autoComplete='off' placeholder='email'
+            value={user.email} onChange={handleInput}
+            />
+            <label htmlFor="password">password</label>
+            <input type="password" name="password" id="password" 
+            required autoComplete='off' placeholder='password'
+            value={user.password} onChange={handleInput}
+            />
+            <br />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </main>
+    </>
+    )
+
 }
 
 export default Login
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
-  return (
-    <div>Register</div>
+
+  const [user,setUser] = useState({
+    username:'Johndoe',
+    email:'johndoe@yahoo.com',
+    password:'1234567890',
+    phone:1234567899
+  })
+
+  //handling input
+  const handleInput = (e) => {
+    console.log(e);
+    console.log('ahgdj');
+    const name = e.target.name;
+    const val = e.target.value;
+    setUser({...user,[name]:val})
+
+  }
+
+  return (<>
+  <main>
+    <div className="section-registration">
+      <div className="register-form">
+        <h1>Registration form</h1>
+        <form >
+          <label htmlFor="username">username</label>
+          <input type="text" name="username" id="username" 
+          required autoComplete='off' placeholder='username'
+          value={user.username} onChange={handleInput}
+          />
+          <label htmlFor="email">email</label>
+          <input type="email" name="email" id="email" 
+          required autoComplete='off' placeholder='email'
+          value={user.email} onChange={handleInput}
+          />
+          <label htmlFor="password">password</label>
+          <input type="password" name="password" id="password" 
+          required autoComplete='off' placeholder='password'
+          value={user.password} onChange={handleInput}
+          />
+          <label htmlFor="phone">phone</label>  
+          <input type="number" name="phone" id="phone" 
+          required autoComplete='off' placeholder='phone'
+          value={user.phone} onChange={handleInput}
+          />
+          <br />
+          <button type="submit" >submit</button>
+        </form>
+      </div>
+    </div>
+  </main>
+  </>
   )
 }
 
